16f76225a525e49242a7ae336319349635fab98e
User <User@192.168.0.13>
2016-03-29 02:36:12 +0200

    Cambiamos el nombre del autor

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Miguel Pérez",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
